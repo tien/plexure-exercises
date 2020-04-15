71d36a6808c02ba20cd384f28c099245d4e5098a
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2020-04-15 18:55:06 +1200

    Update coupon model

--- src/CouponApi/Models/Coupon.cs
+++ src/CouponApi/Models/Coupon.cs
@@ -1,4 +1,13 @@
+using System;
+
 namespace PlexureExercises.CouponApi.Models
 {
-    public class Coupon {}
+    public class Coupon {
+        Guid Id { get; set; }
+        string Title { get; set; }
+        DateTime StartDate { get; set; }
+        DateTime EndDate { get; set; }
+        int MaxCoupons { get; set; }
+        int MaxCouponsPerUser { get; set; }
+    }
 }
